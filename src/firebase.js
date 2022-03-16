e7d2ec084a94d0f5d4b5a8f6717ef9c213006b00
Daniel Hampton <neomachina2011@gmail.com>
2022-03-16 12:11:32 -0500

    fix: update build script and firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: env.API_KEY,
-  authDomain: env.AUTH_DOMAIN,
-  databaseURL: env.DATABASE_URL,
-  projectId: env.PROJECT_ID,
-  storageBucket: env.STORAGE_BUCKET,
-  messagingSenderId: env.MESSAGING_SENDER_ID,
-  appId: env.APP_ID
+  apiKey: process.env.API_KEY,
+  authDomain: 'awkward-astronaut-api.firebaseapp.com',
+  databaseURL: 'https://awkward-astronaut-api-default-rtdb.firebaseio.com/',
+  projectId: 'awkward-astronaut-api',
+  storageBucket: 'awkward-astronaut-api.appspot.com',
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 const hiScoreDiv = document.getElementById('hi-score');
