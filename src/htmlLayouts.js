681c97d1b3fbaa6ef365420d84ad195f9ecede3c
Daniel Hampton <neomachina2011@gmail.com>
2022-04-26 14:28:17 -0500

    feat: update initials and input to twitter handle

--- src/htmlLayouts.js
+++ src/htmlLayouts.js
@@ -11,7 +11,11 @@ export function getHiScoreLayout(hiScoreValues) {
     hiScoreHTML += `
         <tr>
           <td class="placeNumber">${parseInt(entry) + 1}.</td>
-          <td class="initials">${Object.keys(hiScoreValues[entry])[0]}</td>
+          <td class="initials">
+            <a href="https://twitter.com/${Object.keys(hiScoreValues[entry])[0]}">
+              @${Object.keys(hiScoreValues[entry])[0]}
+            </a>
+          </td>
           <td class="dash">-</td>
           <td class="score">${Object.values(hiScoreValues[entry])[0]}</td>
         </tr>
@@ -25,9 +29,9 @@ export function getInitialsInput(gameScore) {
   return `
     <span class="new-hi-score">New Hi Score!</span>
     <span class="game-score">${gameScore}!!!</span>
-    <span class="input-initials">Input up to 3 Initials</span>
+    <span class="input-initials">Input up your Twitter handle (do not add the @ symbol)</span>
     <div class="cursor">
-      <input type="text" id="initials" minlength="1" maxlength="3" pattern="[A-Za-z]{1,3}" required />
+      <input type="text" id="initials" minlength="1" required />
     </div>
     <span class="press-enter">Press Enter to Submit</span>
     `;
